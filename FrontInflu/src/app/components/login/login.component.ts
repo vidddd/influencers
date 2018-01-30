d5e8043fa27d6a20f2604a2870c208841c90d0db
David Álvarez <davidalvarezcalvo@gmail.com>
2018-01-30 09:34:36 +0100

    new api

--- FrontInflu/src/app/components/login/login.component.ts
+++ FrontInflu/src/app/components/login/login.component.ts
@@ -11,15 +11,21 @@ export class LoginComponent {
 
     public user = new User('','');
     public errorMsg = '';
+    private email = '';
+    private password = '';
 
-    constructor(
-        private _service:AuthenticationService) {}
+    constructor(private _service:AuthenticationService) {
+
+    }
 
     login() {
-       if(!this._service.login(this.user)){
+       if(!this._service.login(this.email, this.password)){
+          //  console.log("FAILED LOGIN");
             this.errorMsg = 'Failed to login';
         } else {
+           console.log("LOGIN OK");
            window.location.reload();
         }
+
     }
 }
--- FrontInflu/src/app/services/authentication.service.ts
+++ FrontInflu/src/app/services/authentication.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Router, ActivatedRoute, Params } from '@angular/router';
+import { Http, Headers, Response, RequestOptions } from '@angular/http';
 
 export class User {
   constructor(
@@ -15,25 +16,31 @@ var users = [
 @Injectable()
 export class AuthenticationService {
 
+  urlLogin:string = "https://influencers.tbwainnovation.com/api/users/authenticate";
+
   constructor(
-    private _router: Router){}
+    private _router: Router, private http:Http){}
 
   logout() {
     localStorage.removeItem("user");
         window.location.reload();
   }
 
-  login(user){
-    var authenticatedUser = users.find(u => u.email === user.email);
+  login(username: string, password: string) {
 
-    if (authenticatedUser && authenticatedUser.password === user.password){
-      localStorage.setItem("user", JSON.stringify(authenticatedUser));
-      this._router.navigate(['home']);
-      return true;
-    }
-    return false;
+    this.http.post(this.urlLogin, { username: username, password: password })
+             .toPromise()
+             .then(res => {
+                    if(res.status === 200) {
+                        localStorage.setItem('user', JSON.stringify(res.json()));
+                        return true;
+                    } else {
 
-  }
+                      return false;
+                    }
+                      //console.info(res.json());
+          });
+      }
 
    checkCredentials(){
     if (localStorage.getItem("user") === null){
